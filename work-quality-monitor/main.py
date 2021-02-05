0e6a8003ff7fdd9ad5830b3446bdf59b67007350
Erik Nomitch <enomitch@gmail.com>
2021-02-05 08:25:18 -0600

    + http_get, repl

--- work-quality-monitor/main.py
+++ work-quality-monitor/main.py
@@ -37,6 +37,20 @@ def leds_off():
     np.write()
     return np
 
+def http_get(url):
+    import socket
+    _, _, host, path = url.split('/', 3)
+    addr = socket.getaddrinfo(host, 80)[0][-1]
+    s = socket.socket()
+    s.connect(addr)
+    s.send(bytes('GET /%s HTTP/1.0\r\nHost: %s\r\n\r\n' % (path, host), 'utf8'))
+    while True:
+        data = s.recv(100)
+        if data:
+            print(str(data, 'utf8'), end='')
+        else:
+            break
+    s.close()
 
 def demo(np):
     n = np.n
@@ -91,4 +105,7 @@ time.sleep(2)
 
 leds_off()
 
-demo(np)
+http_get('http://micropython.org/ks/test.html')
+
+
+# demo(np)
