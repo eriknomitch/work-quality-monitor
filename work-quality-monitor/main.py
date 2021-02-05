45ccc28ff165aff3f6a6bcb1dbb35bf91d1cf274
Erik Nomitch <enomitch@gmail.com>
2021-02-04 20:06:45 -0600

    Add main.py with demo

---
+++ work-quality-monitor/main.py
@@ -0,0 +1,56 @@
+import machine, neopixel, time
+
+LED_DATA_PIN = 2
+LED_COUNT = 24
+
+np = neopixel.NeoPixel(machine.Pin(LED_DATA_PIN), LED_COUNT)
+
+
+def leds_off():
+    for i in range(LED_COUNT):
+        np[i] = (0, 0, 0)
+    np.write()
+    return np
+
+
+def demo(np):
+    n = np.n
+
+    # cycle
+    for i in range(4 * n):
+        for j in range(n):
+            np[j] = (0, 0, 0)
+        np[i % n] = (255, 255, 255)
+        np.write()
+        time.sleep_ms(25)
+
+    # bounce
+    for i in range(4 * n):
+        for j in range(n):
+            np[j] = (0, 0, 128)
+        if (i // n) % 2 == 0:
+            np[i % n] = (0, 0, 0)
+        else:
+            np[n - 1 - (i % n)] = (0, 0, 0)
+        np.write()
+        time.sleep_ms(60)
+
+    # fade in/out
+    for i in range(0, 4 * 256, 8):
+        for j in range(n):
+            if (i // 256) % 2 == 0:
+                val = i & 0xFF
+            else:
+                val = 255 - (i & 0xFF)
+            np[j] = (val, 0, 0)
+        np.write()
+
+    # clear
+    for i in range(n):
+        np[i] = (0, 0, 0)
+    np.write()
+
+
+leds_off()
+
+demo(np)
